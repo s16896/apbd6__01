97f6409e5a8d1732062f115430b1e1b7190b2925
s16896 <s16896@pjwstk.edu.pl>
2024-06-02 12:08:07 +0200

    unsaved changes

--- Controllers/HospitalController.cs
+++ Controllers/HospitalController.cs
@@ -1,12 +1,82 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using apbd6.DTOs;
+using apbd6.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using apbd6.DTOs;
+using apbd6.Repositories.Interfaces;
 
 namespace apbd6.Controllers
 {
-    public class HospitalController : Controller
+    [Route("api/hospital")]
+    [ApiController]
+    [Authorize]
+    public class HospitalController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IHospitalDbRepository repository;
+
+        public HospitalController(IHospitalDbRepository repository)
         {
-            return View();
+            this.repository = repository;
+        }
+
+        [HttpGet("doctors")]
+        public async Task<IActionResult> GetDoctors()
+        {
+            var result = await repository.GetDoctorsAsync();
+
+            if (result == null)
+                return NoContent();
+
+            return Ok(result);
+        }
+
+        [HttpPost("doctors")]
+        public async Task<IActionResult> AddDoctor([FromBody] DoctorDto dto)
+        {
+            var result = await repository.AddDoctorAsync(dto);
+
+            if (result != "Success!")
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+
+        [HttpPut("doctors/{id}")]
+        public async Task<IActionResult> ChangeDoctor([FromRoute] int id, [FromBody] DoctorDto dto)
+        {
+            var result = await repository.ChangeDoctorAsync(id, dto);
+
+            if (result != "Success!")
+                return NotFound(result);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("doctors/{id}")]
+        public async Task<IActionResult> DeleteDoctor([FromRoute] int id)
+        {
+            var result = await repository.DeleteDoctorAsync(id);
+
+            if (result != "Success!")
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+
+        [HttpGet("prescriptions/{id}")]
+        public async Task<IActionResult> GetPrescription([FromRoute] int id)
+        {
+            var result = await repository.GetPrescriptionAsync(id);
+
+            if (result == null)
+                return NoContent();
+
+            return Ok(result);
         }
     }
 }
